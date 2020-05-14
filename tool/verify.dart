933cce287124f24ca9e3681776526aa73ae1d328
Devon Carew <devoncarew@google.com>
2020-05-14 13:10:45 -0700

    verify the grammar file on travis

---
+++ tool/verify.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  // Validate the grammars/dart.json file.
+  File grammarFile = File('grammars/dart.json');
+
+  try {
+    jsonDecode(grammarFile.readAsStringSync());
+    print('${grammarFile.path} ok.');
+    exit(0);
+  } catch (e) {
+    print('Error parsing ${grammarFile.path}: $e');
+    exit(1);
+  }
+}
