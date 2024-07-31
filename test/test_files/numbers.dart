51da260fc3231a529cb6525dbf7f0c812e2f24f9
Danny Tuppeny <danny@tuppeny.com>
2024-07-31 14:46:16 +0100

    Add some number tests without the grammar change

---
+++ test/test_files/numbers.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+var a = [
+  1,
+  .0,
+  1.2,
+  0x123,
+  100__000_000__000_000__000_000,
+  0x4000_0000_0000_0000,
+  0.000_000_000_01,
+  0x00_14_22_01_23_45,
+  555_123_4567,
+];
